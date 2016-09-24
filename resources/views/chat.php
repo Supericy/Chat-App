822ce20701157b9fc3d4324859481e097992a8f6
Chad Kosie <chad.kosie@gmail.com>
2016-09-23 23:03:59 -0500

    fudging everything up

--- resources/views/chat.php
+++ resources/views/chat.php
@@ -10,33 +10,76 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/login-modal.css">
 
-<!--    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.18.1/URI.min.js"></script>-->
-<!--    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.14.1/moment.min.js"></script>-->
-<!---->
-<!--    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.0/knockout-min.js"></script>-->
-<!--    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.0.js"></script>-->
-<!--    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
-<!---->
-<!--    <script type="text/javascript" src="https://js.pusher.com/3.2/pusher.js"></script>-->
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-<!--    <script src='http://cdnjs.cloudflare.com/ajax/libs/nicescroll/3.5.4/jquery.nicescroll.js'></script>-->
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
 
+    <!-- Compiled and minified JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+
+    <!--Let browser know website is optimized for mobile-->
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>-->
+
+    <style>
+        header, main, footer {
+            padding-left: 300px;
+        }
+
+        @media only screen and (max-width: 992px) {
+            header, main, footer {
+                padding-left: 0;
+            }
+        }
+    </style>
 </head>
 <body>
 
 <login params="skipAuth: skipAuth, onAuthSuccess: $root.init.bind($root)"></login>
 
-<div class="ui">
-    <div class="left-menu">
-        <channel-list params="channels: channels, channel: channel, me: me"></channel-list>
-        <user-list params="channel: channel"></user-list>
-    </div>
+<header>
+    <ul id="nav-mobile" class="side-nav full fixed">
+<!--        <li>-->
+<!--            <div class="userView">-->
+<!--                <a href="#!user"><img class="circle" src="images/no-user-image.gif"></a>-->
+<!--                <a href="#!name"><span class="name">Unknown</span></a>-->
+<!--                <a href="#!email"><span class="email">jdandturk@gmail.com</span></a>-->
+<!--            </div>-->
+<!--        </li>-->
 
+        <li><div class="divider"></div></li>
+        <channel-list params="channels: channels, channel: channel, me: me"></channel-list>
+
+        <li><div class="divider"></div></li>
+        <user-list params="channel: channel"></user-list>
+    </ul>
+</header>
+
+<main class="ui">
     <chat-pane class="chat" params="channel: channel, me: me"></chat-pane>
-</div>
+</main>
+
+
+<!--<div class="ui">-->
+
+<!--    <div class="left-menu">-->
+<!--        <channel-list params="channels: channels, channel: channel, me: me"></channel-list>-->
+<!--        <user-list params="channel: channel"></user-list>-->
+<!--    </div>-->
+
+<!--    <chat-pane class="chat" params="channel: channel, me: me"></chat-pane>-->
+<!--</div>-->
 
 <script src="/scripts/vendor-bundle.js"></script>
 <script src="/scripts/app-bundle.js"></script>
+<script>
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    $('.collapsible').collapsible();
+</script>
 
 </body>
 </html>
